7f4d8b66c8b02acb6c443b076dfc769496aeca26
Trevor Armand <34165954+trevarmand@users.noreply.github.com>
2019-02-17 21:08:10 -0500

    First Command Line Functionality & More Insights
    Now allows the user to call for specific stats with specified inputs, for example: what is my top song artist x? top album for artist x? top song from album y? etc. 
    Functions run super quickly, beginning to abandon the idea of using SQL since all the computations I've been doing are plenty quick.
    
    TODO:
    Functions need some protection against overlapping song names. It's pretty rare so overlapping albums shouldn't be an issue, but for example "Demon" has been listened to from both Lil Wayne and Quinn XCII
    
    COMING:
    Top Lists
    One play wonders

--- DataCollector.py
+++ DataCollector.py
@@ -13,7 +13,7 @@ class DataCollector:
 
     def __init__(self):
         import time
-        target = "tarmander13"  # input("\nEnter last.fm username: ")
+        target = input("\nEnter last.fm username: ")
         url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + target + \
               "&api_key=" + api_key + "&format=json&limit=200"
         url_data = urllib.request.urlopen(url)
@@ -21,14 +21,13 @@ class DataCollector:
         total_pages = int(self.parseable_data['recenttracks']['@attr']['totalPages'])
         print("Total Pages: " + str(total_pages))
 
-        api_totaltime = 0
+        api_total_time = 0
 
         # Start at 2: We have to call recent tracks to determine how many pages to fetch first page is already loaded.
         # Note: API pages start at 1, not 0
         i = 2
         fail_attempts = 0
-        # Just fetch 5 pages for now. Proof of concept
-        while i < total_pages:  # total_pages:
+        while i <= total_pages:  # total_pages:
             start = time.time()
             url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="\
                   + target + "&page=" + str(i) + \
@@ -38,24 +37,45 @@ class DataCollector:
             except urllib.error.HTTPError:
                 if fail_attempts > 5:
                     print("Server Error, loading failed")
+                    fail_attempts += 1
                 else:
                     time.sleep(0.5)
                     print("Server issue fetching page, retrying: " + str(fail_attempts) + "/5")
                     continue  # Start the loop again, i doesn't increase so just retries the current page
             temp_data = json.load(url_data)
-            api_totaltime += time.time() - start
+            api_total_time += time.time() - start
             temp_data['recenttracks']['track'].pop(0)  # Don't include current track in data
             self.parseable_data['recenttracks']['track'].extend(temp_data['recenttracks']['track'])
             print("Loaded page " + str(i))
             i += 1
-        print("Average api response time: " + str(round(api_totaltime / (i - 1), 3)))
+        print("\nAverage api response time: " + str(round(api_total_time / (i - 1), 3)))
 
     def main(self):
         self.tracks_loaded = len(self.parseable_data['recenttracks']['track'])
-        print("Tracks loaded: " + str(self.tracks_loaded))
-        self.most_played_song()
-        self.most_played_artist()
-        self.played_on_repeat()
+        print("Tracks loaded: " + str(self.tracks_loaded) + "\n")
+        self.take_input()
+
+    # Prompt the user for input so they can gather insights on their listening
+    def take_input(self):
+        next_in = input("Enter command, or HELP for help: ")
+        if next_in.lower() == 'help':
+            print("\nCommands available:\ntop_track\ntop_artist\non_repeat"
+                  "\nsong_play_count (song)\ntrack_by_artist (artist)\nalbum_by_artist (artist)\nsong_by_album (album)")
+        elif next_in == 'top_track':
+            self.most_played_song()
+        elif next_in == 'top_artist':
+            self.most_played_artist()
+        elif next_in == 'on_repeat':
+            self.played_on_repeat()
+        elif next_in.__contains__('song_play_count'):
+            self.song_play_count(next_in[16:])
+        elif next_in.__contains__('track_by_artist'):
+            self.top_track_by_artist(next_in[14:])
+        elif next_in.__contains__('album_by_artist'):
+            self.album_by_artist(next_in[16:])
+        elif next_in.__contains__('song_by_album'):
+            self.song_by_album(next_in[14:])
+        self.take_input()
 
     # Prints the most played song by artist and number of times
     def most_played_song(self):
@@ -71,7 +91,7 @@ class DataCollector:
                 top_artist = track['artist']['#text']
                 last_count = track_counts.get(cur_track)
         print("\nTop track: " + str(top_track) + " by " + top_artist)
-        print("Number of plays: " + str(last_count))
+        print("Number of plays: " + str(last_count) + "\n")
 
     # Prints the most played artist and number of plays, and the % of total plays they owned
     def most_played_artist(self):
@@ -86,7 +106,7 @@ class DataCollector:
                 top_count = artist_counts.get(artist_name, 0)
         print("\nTop artist: " + top_artist)
         print("Number of plays: " + str(top_count))
-        print(top_artist + " consumed " + str(round(top_count / self.tracks_loaded, 3) * 100) + "% of your listening")
+        print(top_artist + " consumed " + str(round(top_count / self.tracks_loaded, 3) * 100) + "% of your listening\n")
 
     # Prints the most repeated song and the number of repeats
     def played_on_repeat(self):
@@ -108,7 +128,65 @@ class DataCollector:
                 cur_count = 0
             prev_track = track['name']
         print("\nMost Repeated: " + str(most_repreated) + " by " + top_artist)
-        print("Number of plays: " + str(last_count))
+        print("Number of plays: " + str(last_count) + "\n")
+
+    # Prints your most played track for the specified artist
+    def top_track_by_artist(self, artist):
+        track_counts = {}
+        last_count = 0
+        top_track = "Null"
+        for track in self.parseable_data['recenttracks']['track']:
+            if track['artist']['#text'].lower() == artist.lower():
+                cur_track = track['name']
+                track_counts[cur_track] = track_counts.get(cur_track, 0) + 1
+                if track_counts.get(cur_track) > last_count:
+                    top_track = cur_track
+                    last_count = track_counts.get(cur_track)
+        if last_count == 0:
+            print("No tracks found for " + artist)
+        else:
+            print("\nTop track for " + artist + ": " + str(top_track))
+            print("Number of plays: " + str(last_count) + "\n")
+
+    # Prints the number of times you played the specified song
+    def song_play_count(self, song):
+        count = 0
+        for track in self.parseable_data['recenttracks']['track']:
+            if track['name'].lower() == song.lower():
+                count += 1
+        print("You listened to " + song + " " + str(count) + " times\n")
+
+    # Tells you your favorite album from the specified artist
+    def album_by_artist(self, artist):
+        album_counts = {}
+        top_album = "null"
+        cur_max = 0
+        for track in self.parseable_data['recenttracks']['track']:
+            if track['artist']['#text'].lower() == artist.lower():
+                album_counts[track['album']['#text']] = album_counts.get(track['album']['#text'], 0) + 1
+                if album_counts[track['album']['#text']] > cur_max:
+                    top_album = track['album']['#text']
+                    cur_max = album_counts[track['album']['#text']]
+        if cur_max == 0:
+            print("You haven't listened to this artist")
+        else:
+            print("Your top album from " + artist + " is " + top_album + ", with " + str(cur_max) + " listens\n")
+
+    # Tells you your favorite song from a given album
+    def song_by_album(self, album):
+        song_counts = {}
+        top_song = "null"
+        cur_max = 0
+        for track in self.parseable_data['recenttracks']['track']:
+            if track['album']['#text'].lower() == album.lower():
+                song_counts[track['name']] = song_counts.get(track['name'], 0) + 1
+                if song_counts[track['name']] > cur_max:
+                    top_song = track['name']
+                    cur_max = song_counts[track['name']]
+        if cur_max == 0:
+            print("You haven't listened to this album")
+        else:
+            print("Your top track from " + album + " is " + top_song + " with " + str(cur_max) + " plays\n")
 
 
 dc = DataCollector()
